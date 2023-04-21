47ada1ff4374c5cb96cc7d63281180d80f209f3d
RobinsonPenagos <103594345+RobinsonPenagos@users.noreply.github.com>
2023-04-21 00:31:38 -0500

    update react

--- containers/CrudContainer.js
+++ containers/CrudContainer.js
@@ -1,47 +1,187 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import { Table, Col, Row, Button, Modal, Input } from "antd";
+const { TextArea } = Input;
+
 const CrudContainer = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+
+  const [form, setForm] = useState({
+    name: "",
+    price: "",
+    description: "",
+  });
 
   useEffect(() => {
-    fetchdata();
+    fetchData();
   }, []);
-  
-  const fetchdata = async () => {
-    const response = await axios.get("http://localhost:3001/products/");
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const showModal = () => {
+    setOpenModal(true);
+    resetForm();
+    setOpenModal(true);
+  };
+  const handleOk = () => {
+    setOpenModal(false);
+  };
+  const handleCancel = () => {
+    setOpenModal(false);
+  };
+
+  const fetchData = async () => {
+    const response = await axios.get("https://simple-mysql-api-production.up.railway.app/products/");
     setData(response.data);
   };
+
+  const handlePost = async () => {
+    const response = await axios.post("https://simple-mysql-api-production.up.railway.app/products/", form);
+    if (response.status === 200) {
+      alert(response.data.message);
+    } else {
+      alert("Producto no ha sido creado");
+    }
+    console.log("response post", response);
+    setOpenModal(false);
+    fetchData();
+  };
+
+  const handleEdit = (record) => {
+    setForm(record);
+    setOpenModal(true);
+  };
+
+  const handleUpdate = async () => {
+    const response = await axios.put(
+      `https://simple-mysql-api-production.up.railway.app/products/${form.id}`,
+      form
+    );
+    if (response.status === 200) {
+      alert(response.data.message);
+    } else {
+      alert("Producto no ha sido actualizado");
+    }
+    console.log("response put", response);
+    setOpenModal(false);
+    fetchData();
+  };
+
+  
+  const handleDelete = async (id) => {
+    const response = await axios.delete(
+      `https://simple-mysql-api-production.up.railway.app/products/${id}`
+    );
+    if (response.status === 200) {
+      alert(response.data.message);
+    } else {
+      alert("Producto no ha sido eliminado");
+    }
+    console.log("response delete", response);
+    fetchData();
+  };
+
+  const columns = [
+    {
+      title: "#",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "Nombre",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Precio",
+      dataIndex: "price",
+      key: "price",
+    },
+    {
+      title: "Descripcion",
+      dataIndex: "description",
+      key: "description",
+    },
+    {
+      title: "Editar",
+      dataIndex: "",
+      key: "x",
+      render: (record) => (
+        <Button type="primary" onClick={() => handleEdit(record)}>
+          Editar
+        </Button>
+      ),
+    },
+    {
+      title: "Eliminar",
+      dataIndex: "",
+      key: "y",
+      render: (record) => (
+        <Button type="primary" danger onClick={() => handleDelete(record.id)}>
+          Eliminar
+        </Button>
+      ),
+    },
+  ];
+
+  const resetForm = () => {
+    setForm({
+      name: "",
+      price: "",
+      description: "",
+    });
+  };
+
   return (
     <>
-      <h1>Mi primera api</h1>
-      <table border="solid">
-        <thead>
-          <tr>
-            <td>id</td>
-            <td>name</td>
-            <td>price</td>
-            <td>description</td>
-          </tr>
-        </thead>
-        <tbody>
-          {data ? (
-            data.map((item) => (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.name}</td>
-                <td>{item.price}</td>
-                <td>{item.description}</td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="4">No hay data</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+      <Row>
+        <Col span={12} offset={6}>
+          <h1>Mi primera api</h1>
+          <Button type="primary" onClick={showModal}>
+            Crear
+          </Button>
+          <Table rowKey={"id"} dataSource={data} columns={columns} />
+        </Col>
+      </Row>
+
+      <Modal
+        title={form.id ? "Editar producto" : "Crear producto"}
+        open={openModal}
+        onOk={form.id ? handleUpdate : handlePost}
+        onCancel={() => setOpenModal(false)}
+      >
+        <p>Nombre:</p>
+        <Input
+          onChange={handleChange}
+          name="name"
+          placeholder="Nombre del producto"
+          value={form && form.name}
+        />
+        <p>Precio:</p>
+        <Input
+          onChange={handleChange}
+          name="price"
+          placeholder="Precio del producto"
+          value={form && form.price}
+        />
+        <p>Descripcion:</p>
+        <TextArea
+          onChange={handleChange}
+          name="description"
+          value={form && form.description}
+          placeholder="Descripcion del producto"
+          rows={4}
+          maxLength={25}
+        />
+      </Modal>
     </>
   );
 };
